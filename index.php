be23e1c26cbe1f7a014a7e5a5f4111875cc8a638
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-04-17 21:39:01 +0500

    database refractor

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 require_once "functions.php";
 require_once "Database.php";
-
 // require_once "router.php";
 
-$db = new Database();
+$config = require_once "config.php";
+
+$db = new Database($config['database']);
 $posts = $db->query("select * from posts where id")->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
