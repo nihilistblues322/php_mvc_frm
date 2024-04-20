5652a4cd8318941f3cd15fc471a6973ddecd474e
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-04-20 19:36:26 +0500

    simple auth, db +

--- controllers/note.php
+++ controllers/note.php
@@ -4,14 +4,9 @@ $config = require_once "config.php";
 $db = new Database($config['database']);
 $heading = 'Note';
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+authorize($note['user_id'] ==  5);
 
-if (!$note) {
-    abort();
-}
 
-if ($note['user_id'] !== 4) {
-    abort(Response::FORBIDDEN);
-}
 
 require "views/note.view.php";
--- controllers/notes.php
+++ controllers/notes.php
@@ -6,10 +6,8 @@ $db = new Database($config['database']);
 
 $heading = 'Notes';
 $notes = [];
-$notes = $db->query('select * from notes where user_id = 4')->fetchAll();
+$notes = $db->query('select * from notes where user_id = 5')->get();
 
 
 
 require "views/notes.view.php";
-
-
