c5e545fa404a6eccf711fdce8f30cc71f5c33133
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-04-21 15:14:10 +0500

    basic validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,18 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 5,
-    ]);
+    $errors = [];
+    if (strlen($_POST['body']) == 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (empty($errors)) {
+
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 5,
+        ]);
+    }
 }
 
 require_once 'views/note-create.view.php';
