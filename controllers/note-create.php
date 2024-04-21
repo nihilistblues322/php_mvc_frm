c1e40e0e2c550604b63c23c557825592ffb2d309
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-04-21 14:23:21 +0500

    post query + html special chars

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,7 +1,15 @@
 <?php
+
+$config = require_once "config.php";
+$db = new Database($config['database']);
+
 $heading = 'Create Note';
-//if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    //dd($_POST);
-//}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 5,
+    ]);
+}
 
 require_once 'views/note-create.view.php';
